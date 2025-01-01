f5f8404c4161fca3b08cf0833c414f39afb9c10e
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2025-01-01 06:56:30 -0500

    feat: Add image to Hero1735703512

--- src/ProjectComponents/Hero1735703512/component.tsx
+++ src/ProjectComponents/Hero1735703512/component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero: React.FC = () => {
   
   return (
-    <div className="bg-black py-16 text-white w-full h-full">
+    <div className="bg-black py-16 text-white w-full h-full bg-cover bg-center" style={{ backgroundImage: `url('https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Garbage_Trading_Platform_1735703509/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/9f9a3a4a8804432799f329c5d6ba48bb.jpeg')` }}>
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center h-full">
         <div className="md:w-1/2 mb-8 md:mb-0">
           <h1 className="text-4xl font-bold mb-4">Turn Your Trash into Treasure</h1>
